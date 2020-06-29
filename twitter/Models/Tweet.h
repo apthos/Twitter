dc61909411f8908fc76b56b824fc5bad6c0cc867
apthos <david00lara@gmail.com>
2020-06-29 16:29:00 -0400

    Added user and tweet models

---
+++ twitter/Models/Tweet.h
@@ -0,0 +1,33 @@
+//
+//  Tweet.h
+//  twitter
+//
+//  Created by David Lara on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "User.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tweet : NSObject
+
+@property (nonatomic, strong) NSString *idString;
+@property (nonatomic, strong) NSString *text;
+@property (nonatomic) int favoriteCount;
+@property (nonatomic) BOOL favorited;
+@property (nonatomic) int retweetCount;
+@property (nonatomic) BOOL retweeted;
+@property (nonatomic, strong) User *user;
+@property (nonatomic, strong) NSString *createdAtString;
+
+@property (nonatomic, strong) User *retweetedByUser;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Models/Tweet.m
@@ -0,0 +1,59 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by David Lara on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        
+        //Checks if tweet is a retweet
+        NSDictionary *originalTweet = dictionary[@"retweeted_status"];
+        if (originalTweet != nil) {
+            NSDictionary *userDictionary = dictionary[@"user"];
+            self.retweetedByUser = [[User alloc] initWithDictionary:userDictionary];
+            
+            //Changes tweet to original tweet for posting
+            dictionary = originalTweet;
+        }
+        
+        self.idString = dictionary[@"id_str"];
+        self.text = dictionary[@"text"];
+        self.favoriteCount = [dictionary[@"favorite_count"] intValue];
+        self.favorited = [dictionary[@"favorited"] boolValue];
+        self.retweetCount = [dictionary[@"retweet_count"] intValue];
+        self.retweeted = [dictionary[@"retweeted"] boolValue];
+        
+        NSDictionary *user = dictionary[@"user"];
+        self.user = [[User alloc] initWithDictionary:user];
+        
+        //Formats the createdAt string
+        NSString *createdAtOriginalString = dictionary[@"created_at"];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+        NSDate *date = [formatter dateFromString:createdAtOriginalString];
+        formatter.dateStyle = NSDateFormatterShortStyle;
+        formatter.timeStyle = NSDateFormatterNoStyle;
+        self.createdAtString = [formatter stringFromDate:date];
+        
+    }
+    return self;
+}
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries {
+    NSMutableArray *tweets = [NSMutableArray array];
+    for (NSDictionary *dictionary in dictionaries) {
+        Tweet *tweet = [[Tweet alloc] initWithDictionary:dictionary];
+        [tweets addObject:tweet];
+    }
+    return tweets;
+}
+
+@end
